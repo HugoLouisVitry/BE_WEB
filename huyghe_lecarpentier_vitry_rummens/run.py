3d7fe315362988d22296631155eaf1875c17b23c
Mathias_Huyghe <mathuyhuy@gmail.com>
2023-04-19 13:36:22 +0200

    jsp

---
+++ huyghe_lecarpentier_vitry_rummens/run.py
@@ -0,0 +1,7 @@
+from App_financement.config import DEBUG, WEB_SERVER
+from App_financement.views import app
+
+if __name__=='__main__':
+    app.run ( host=WEB_SERVER['host'],
+            port=WEB_SERVER['port'],
+            debug=DEBUG)
