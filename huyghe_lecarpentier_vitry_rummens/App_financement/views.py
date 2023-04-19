eec565ca804d6caf8c231ae763e9228c8f75b494
Mathias_Huyghe <mathuyhuy@gmail.com>
2023-04-19 13:45:51 +0200

    Jinja

--- huyghe_lecarpentier_vitry_rummens/App_financement/views.py
+++ huyghe_lecarpentier_vitry_rummens/App_financement/views.py
@@ -1,6 +1,9 @@
 from flask import Flask, render_template
 
 app=Flask(__name__)
+app.template_folder = "template"
+app.static_folder = "static"
+app.config.from_object('App_financement.config')
 
 #page accueil
 @app.route("/")
