3bd98c14496099bb00dd8f3732b4fd2dbe2b5812
Mathias_Huyghe <mathuyhuy@gmail.com>
2023-04-24 19:05:14 +0200

    Pages de co

--- huyghe_lecarpentier_vitry_rummens/App_financement/views.py
+++ huyghe_lecarpentier_vitry_rummens/App_financement/views.py
@@ -14,9 +14,14 @@ def index():
 def about():
     return render_template("about.html") 
 
-@app.route("/contact")
-def contact():
-    return render_template("contact.html")
+@app.route("/login")
+def login():
+    return render_template("login.html")
+
+@app.route("/signin")
+def signin():
+    return render_template("signin.html")
+
 
 @app.route("/pricing")
 def pricing():
