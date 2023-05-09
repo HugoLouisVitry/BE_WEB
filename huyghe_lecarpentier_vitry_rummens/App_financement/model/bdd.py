54b5897251a0eab0ce2bb534be7c0cbbd57024ab
marie-lecarpentier <marielecarpentier.123@gmail.com>
2023-05-09 09:10:48 +0200

    ouaiiiis

--- huyghe_lecarpentier_vitry_rummens/App_financement/model/bdd.py
+++ huyghe_lecarpentier_vitry_rummens/App_financement/model/bdd.py
@@ -61,25 +61,25 @@ def del_membreData(idUser):
     return 1
 
 #################################################################################
-#ajout d'un membre
-def add_membreData(nom, prenom, mail, login, motPasse, statut, avatar):
-    cnx = connexion() 
-    if cnx is None: 
-        return None
-    try:
-        cursor = cnx.cursor()
-        sql = "INSERT INTO identification (nom, prenom, mail, login, motPasse, statut, avatar) VALUES (%s, %s, %s, %s, %s, %s, %s);"
-        param = (nom, prenom, mail, login, motPasse, statut, avatar)
-        cursor.execute(sql, param)
-        lastId = cursor.lastrowid  # récupère le dernier idUser, généré par le serveur sql
-        cnx.commit()
-        close_bd(cursor, cnx)
-        #session['successDB'] = "OK add_membreData"
-    except mysql.connector.Error as err:
-        lastId = None
-        session['errorDB'] = "Failed add membres data : {}".format(err)
-        print(session['errorDB']) #le problème s'affiche dans le terminal
-    return lastId
+# #ajout d'un membre
+# def add_membreData(nom, prenom, mail, login, motPasse, statut, avatar):
+#     cnx = connexion() 
+#     if cnx is None: 
+#         return None
+#     try:
+#         cursor = cnx.cursor()
+#         sql = "INSERT INTO identification (nom, prenom, mail, login, motPasse, statut, avatar) VALUES (%s, %s, %s, %s, %s, %s, %s);"
+#         param = (nom, prenom, mail, login, motPasse, statut, avatar)
+#         cursor.execute(sql, param)
+#         lastId = cursor.lastrowid  # récupère le dernier idUser, généré par le serveur sql
+#         cnx.commit()
+#         close_bd(cursor, cnx)
+#         #session['successDB'] = "OK add_membreData"
+#     except mysql.connector.Error as err:
+#         lastId = None
+#         session['errorDB'] = "Failed add membres data : {}".format(err)
+#         print(session['errorDB']) #le problème s'affiche dans le terminal
+#     return lastId
 
 #################################################################################
 #modification d'une donnée dans la table identification
@@ -144,13 +144,13 @@ def saveDataFromFile(data):
         print(session['errorDB']) #le problème s'affiche dans le terminal
     return 1
     
-def add_membreData(nom, prenom, mail, login, motPasse, statut, avatar):
+def add_membreData(idUser, nom, prenom, mail, login, password, isAdmin, reponse):
     cnx = connexion()
     if cnx is None: return None
     try:
         cursor = cnx.cursor()
         sql = "INSERT INTO identification (nom, prenom, mail, login, motPasse, statut, avatar) VALUES (%s, %s, %s, %s, %s, %s, %s);"
-        param = (nom, prenom, mail, login, motPasse, statut, avatar)
+        param = (idUser, nom, prenom, mail, login, password, isAdmin, reponse)
         cursor.execute(sql, param)
         lastId = cursor.lastrowid # dernier idUser généré
         cnx.commit()
@@ -159,20 +159,5 @@ def add_membreData(nom, prenom, mail, login, motPasse, statut, avatar):
     except mysql.connector.Error as err:
         lastId = None
         session['errorDB'] = "Failed add membres data : {}".format(err)
+        print(session['errorDB']) #le problème s'affiche dans le terminal
     return lastId
-
-#modification d'une donnée dans la table identification
-def update_membreData(champ, idUser, newvalue):
-    cnx = connexion()
-    if cnx is None: return None
-    try:
-        cursor = cnx.cursor()
-        sql = "UPDATE identification SET "+champ+" = %s WHERE idUser = %s;"
-        param = (newvalue, idUser)
-        cursor.execute(sql, param)
-        cnx.commit()
-        close_bd(cursor, cnx)
-        #session['successDB'] = "OK update_membreData"
-    except mysql.connector.Error as err:
-        session['errorDB'] = "Failed update membres data : {}".format(err)
-    return 1
